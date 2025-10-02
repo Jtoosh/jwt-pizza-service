0a5500db2c8ecb7d9a9fe35dd0fd2df79b79f87c
James Teuscher <james.teuscher@outlook.com>
2025-10-02 15:46:22 -0600

    Update (franchisRouter.test) bug fix

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -1,18 +1,21 @@
 const {Role, DB} = require('../database/database');
 const request = require("supertest");
 const app = require("../service");
+const utils = require('../test.utils.js');
 
 
 function randomName() {
     return Math.random().toString(36).substring(2, 12);
 }
 
-let testAdmin= {};
+let testAdmin;
+let adminUserAuthToken;
 
 beforeAll(async () => {
-    const userName = randomName();
-    testAdmin = {name: userName, email: userName + '@gmail.com', password: 'a', roles: [{role: Role.Admin}]};
-    await DB.addUser(testAdmin);
+    testAdmin = await utils.createAdminUser()
+    const loginRes = await request(app).put('/api/auth/').send(testAdmin);
+    adminUserAuthToken = loginRes.body.token;
+    utils.expectValidJwt(loginRes.body.token);
 
 });
 
@@ -22,11 +25,18 @@ afterAll(async () => {
 
 test('create franchise positive', async () =>{
     const newFranchise = {name: randomName(), admins: [{email: testAdmin.email}]};
-    const loginRes = await request(app).put('/api/auth/login').send(testAdmin);
-    const createRes = await request(app).post('/api/franchise').set('Authorization', `Bearer ${loginRes.body.token}`).send(newFranchise);
+    const createRes = await request(app).post('/api/franchise').set('Authorization', `Bearer ${adminUserAuthToken}`).send(newFranchise);
 
     expect(createRes.status).toBe(200);
     expect(createRes.body.name).toMatch(newFranchise.name);
-    expect(createRes.body.admins.email).toMatch(testAdmin.email);
+    expect(createRes.body.admins[0].email).toMatch(testAdmin.email);
 
 });
+
+test('list franchises', async () =>{
+    const listRes = await request(app).get('api/franchise?page=0&limit=10&name=*');
+
+    expect(listRes.status).toBe(200);
+    expect(listRes.body.franchises.length).toBeGreaterThan(0);
+    expect(listRes.body.franchises[0].name).toMatch(testAdmin.name);
+})
